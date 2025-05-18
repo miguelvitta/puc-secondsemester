16f6798343cd674373af160d983791bdf8d8ec18
miguelvitta <miguelvittadev@gmail.com>
2025-05-18 16:38:34 -0300

    feat: Implements two algorithms of calculating mdc

---
+++ fundamentos-computacao/exercicio-03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int modExp(int base, int exponent, int mod);
+
+int main() {
+    int result = modExp(3, 11, 5);
+    printf("O resultado é %d\n", result);
+
+    return 0;
+}
+
+int modExp(int base, int exponent, int mod) {
+    if (exponent == 0) {
+        return 1;
+    }
+
+    if (exponent % 2 == 0) {
+        int temp = modExp(base, exponent / 2, mod);
+        return (temp * temp) % mod;
+    }
+
+    return (base * modExp(base, exponent - 1, mod)) % mod;
+}
